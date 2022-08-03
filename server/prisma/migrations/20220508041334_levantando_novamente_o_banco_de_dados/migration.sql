62b97f84130b795d16c450d0b37935fa9b5add80
leksansilva <69767375+leksansilva@users.noreply.github.com>
2022-08-03 14:48:56 -0300

    corrigindo novamente

--- server/prisma/migrations/20220508041334_levantando_novamente_o_banco_de_dados/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "feedbacks" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "type" TEXT NOT NULL,
-    "comment" TEXT NOT NULL,
-    "screenshot" TEXT
-);
---
+++ server/prisma/migrations/20220803174735_/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - The primary key for the `feedbacks` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_feedbacks" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "type" TEXT NOT NULL,
+    "comment" TEXT NOT NULL,
+    "screenshot" TEXT
+);
+INSERT INTO "new_feedbacks" ("comment", "id", "screenshot", "type") SELECT "comment", "id", "screenshot", "type" FROM "feedbacks";
+DROP TABLE "feedbacks";
+ALTER TABLE "new_feedbacks" RENAME TO "feedbacks";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
